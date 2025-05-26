7a4fcbf41969645da17e8285198b5e4e24113f9d
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2025-05-25 21:35:50 -0500

    Obteniendo los banners

--- app/src/screens/Home/HomeScreen.js
+++ app/src/screens/Home/HomeScreen.js
@@ -2,9 +2,29 @@ import { Text, SafeAreaView } from 'react-native'
 import {useAuth} from "../../hooks"
 import {Button} from "react-native-paper"
 import {BasicLayout, Layout} from "../../layouts"
+import {useState, useEffect} from "react"
+import { homeBannerCtrl } from '@/src/api'
+import Toast from 'react-native-root-toast'
 
 export function HomeScreen() {
   const {logout} = useAuth()
+  const [banners, setBanners] = useState(null)
+
+  useEffect(() => {
+    getBanners()
+  }, [])
+
+  const getBanners = async () => {
+    try {
+      const response = await homeBannerCtrl.getAll()
+      setBanners("Banners:", response?.data || null)
+    } catch (error) {
+      Toast.show("Error al cargar los banners", {
+        position: Toast.positions.CENTER,
+      })
+    }
+  }
+
   return (
     <SafeAreaView>
       <Layout.Basic showBack={false} textTitleCenter="Inicio">
--- app/src/utils/constants.js
+++ app/src/utils/constants.js
@@ -8,6 +8,7 @@ export const ENV ={
         USERS_ME: "users/me",
         USERS: "users",
         ADDRESSES: "addresses",
+        HOME_BANNERS: "home-banners",
     },
     STORAGE:{
         TOKEN: "token",
