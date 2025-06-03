6bd620f1c5db5a7db195b7acf5baee5c5b987ff5
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2025-06-03 17:11:33 -0500

    chore: clean up index.js by removing unnecessary blank lines; update tsconfig.json to include extends property

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -1,13 +1,7 @@
 export * from './auth';
-
 export * from './storage';
-
 export * from './user';
-
 export * from './address';
-
 export * from './home-banner';
-
 export * from './product';
-
 export * from './search-history';
