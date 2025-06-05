354aeeb3c55d74d74c088e94693f61ecb78d6d92
Camilo Alejandro <79614560+its-camilo@users.noreply.github.com>
2025-06-05 20:14:10 +0000

    fix: unificar comillas en la configuración de middlewares

--- server/dist/config/middlewares.js
+++ server/dist/config/middlewares.js
@@ -1,27 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = [
-    'strapi::logger',
-    'strapi::errors',
-    'strapi::security',
+    "strapi::logger",
+    "strapi::errors",
+    "strapi::security",
     {
-        name: 'strapi::cors',
+        name: "strapi::cors",
         config: {
             enabled: true,
-            headers: '*',
+            headers: "*",
             origin: [
-                'https://organic-xylophone-w4x4p9v7q7xhgj9r-8081.app.github.dev',
-                'https://organic-xylophone-w4x4p9v7q7xhgj9r-3000.app.github.dev',
+                "https://organic-xylophone-w4x4p9v7q7xhgj9r-8081.app.github.dev",
+                "https://organic-xylophone-w4x4p9v7q7xhgj9r-3000.app.github.dev",
                 /https:\/\/.*\.app\.github\.dev$/,
-                'http://localhost:3000',
-                'http://localhost:8081'
-            ]
-        }
+                "http://localhost:3000",
+                "http://localhost:8081",
+            ],
+        },
     },
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
