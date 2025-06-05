41bad1117bbdca5ba04d87ada6804acff989d6c9
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2025-06-04 22:48:41 -0500

    feat: add default APP_KEYS to server configuration for enhanced security

--- server/dist/config/server.js
+++ server/dist/config/server.js
@@ -4,6 +4,11 @@ exports.default = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: {
-        keys: env.array('APP_KEYS'),
+        keys: env.array('APP_KEYS', [
+            'rD9s/qPgR1e4NGYtIne3aA==',
+            '75nGge5PWysPMi/j3RGKjw==',
+            'M0OMznn6eMMP/hTdj6h/Rw==',
+            'TsZDaY3iylBHnVw9bGQNPg=='
+        ]),
     },
 });
