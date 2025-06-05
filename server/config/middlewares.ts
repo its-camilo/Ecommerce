cfb8578f70d4cd707ff30afa00160a6778a02e3d
Camilo Alejandro <79614560+its-camilo@users.noreply.github.com>
2025-06-05 20:12:28 +0000

    fix: corregir la configuración de CORS para permitir el acceso desde dominios específicos

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,25 +1,25 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
   {
-    name: 'strapi::cors',
+    name: "strapi::cors",
     config: {
       enabled: true,
-      headers: '*',
+      headers: "*",
       origin: [
-        'https://organic-xylophone-w4x4p9v7q7xhgj9r-8081.app.github.dev',
-        'https://organic-xylophone-w4x4p9v7q7xhgj9r-3000.app.github.dev',
+        "https://organic-xylophone-w4x4p9v7q7xhgj9r-8081.app.github.dev",
+        "https://organic-xylophone-w4x4p9v7q7xhgj9r-3000.app.github.dev",
         /https:\/\/.*\.app\.github\.dev$/,
-        'http://localhost:3000',
-        'http://localhost:8081'
-      ]
-    }
+        "http://localhost:3000",
+        "http://localhost:8081",
+      ],
+    },
   },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
