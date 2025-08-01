22a3926a3b0e068ddd5835079cfc54ee03455af0
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2025-08-01 18:08:46 -0500

    fix: update middleware configuration for improved security and CORS handling

--- server/dist/config/middlewares.js
+++ server/dist/config/middlewares.js
@@ -1,27 +1,25 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [
-    "strapi::logger",
-    "strapi::errors",
-    "strapi::security",
-    {
-        name: "strapi::cors",
-        config: {
-            enabled: true,
-            headers: "*",
-            origin: [
-                "https://organic-xylophone-w4x4p9v7q7xhgj9r-8081.app.github.dev",
-                "https://organic-xylophone-w4x4p9v7q7xhgj9r-3000.app.github.dev",
-                /https:\/\/.*\.app\.github\.dev$/,
-                "http://localhost:3000",
-                "http://localhost:8081",
-            ],
-        },
-    },
-    "strapi::poweredBy",
-    "strapi::query",
-    "strapi::body",
-    "strapi::session",
-    "strapi::favicon",
-    "strapi::public",
-];
+// export default [
+//   "strapi::logger",
+//   "strapi::errors",
+//   "strapi::security",
+//   {
+//     name: "strapi::cors",
+//     config: {
+//       enabled: true,
+//       headers: "*",
+//       origin: [
+//         "https://organic-xylophone-w4x4p9v7q7xhgj9r-8081.app.github.dev",
+//         "https://organic-xylophone-w4x4p9v7q7xhgj9r-3000.app.github.dev",
+//         /https:\/\/.*\.app\.github\.dev$/,
+//         "http://localhost:3000",
+//         "http://localhost:8081",
+//       ],
+//     },
+//   },
+//   "strapi::poweredBy",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
