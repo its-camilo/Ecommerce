35e9c1026d7af9aa9197f6442765fe203d91f73a
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2025-08-04 19:42:36 -0500

    feat: Enhance app configuration and add GitHub Pages deployment workflow

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -8,9 +8,11 @@ export default [
       enabled: true,
       headers: "*",
       origin: [
+        "https://its-camilo.github.io",
         "https://organic-xylophone-w4x4p9v7q7xhgj9r-8081.app.github.dev",
         "https://organic-xylophone-w4x4p9v7q7xhgj9r-3000.app.github.dev",
         /https:\/\/.*\.app\.github\.dev$/,
+        /https:\/\/.*\.github\.io$/,
         "http://localhost:3000",
         "http://localhost:8081",
       ],
